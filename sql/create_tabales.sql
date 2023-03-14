d5494ac6d66128691ec548df5efbd15a3f9e938e
Yoshihisa19920807 <yoshi19920807@gmail.com>
2023-03-14 22:11:53 +0900

    add table uses

--- sql/create_tabales.sql
+++ sql/create_tabales.sql
@@ -1,8 +1,16 @@
-CREATE TABLE public.good_deeds (
+CREATE TABLE IF NOT EXISTS public.good_deeds (
   id integer not null,
   title character varying(255),
   count integer NOT NULL DEFAULT 0,
   details text,
   created_at timestamp without time zone,
   updated_at timestamp without time zone
-)
+);
+
+CREATE TABLE IF NOT EXISTS public.users (
+  id integer not null,
+  email character varying(255),
+  password character varying(255),
+  created_at timestamp without time zone,
+  updated_at timestamp without time zone
+);
