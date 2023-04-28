6db15667d168d7bfe15a53b9f45b16ade9148cd0
Yoshihisa19920807 <yoshi19920807@gmail.com>
2023-04-29 04:15:44 +0900

    最小api作成not foundだけ返ってくる

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	fmt.Println("Let's count your good deeds!")
 }
