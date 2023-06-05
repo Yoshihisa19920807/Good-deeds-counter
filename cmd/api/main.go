d6324d82c95b3dfa4493588cd072117ff68e7a9b
Yoshihisa19920807 <yoshi19920807@gmail.com>
2023-06-05 10:03:33 +0900

    db接続

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	db, err := sql.Open("postgres", "user=yoshihisaokada dbname=good_deeds_counter sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("SELECT name FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var name string
+		if err := rows.Scan(&name); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(name)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 	fmt.Println("Let's count your good deeds!")
 }
